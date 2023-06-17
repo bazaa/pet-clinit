a24a65403c2566f30ef1b441c7d988d061952e2a
bazaa <bazaalejandro7@gmail.com>
2023-06-16 22:42:14 -0400

    added starter files

---
+++ src/main/java/com/project/petclinit/PetClinitApplication.java
@@ -0,0 +1,13 @@
+package com.project.petclinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/petclinit/PetClinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.petclinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
